1df65303f388ab9949e3e9528f24beadc2899059
Ron Newcomb <ron.newcomb@agilent.com>
2022-11-18 21:16:22 -0800

    state in individual .map items works

--- components/fancify.js
+++ components/fancify.js
@@ -1,6 +1,8 @@
 import { jsx } from "../framework.js";
-export default function ({ x, children }) {
-    return (jsx("div", { style: "font-size: large" },
+export default function ({ x, children }, { useState }) {
+    const [f, setf] = useState("large");
+    const toggle = () => setf(f === "large" ? "small" : "large");
+    return (jsx("div", { style: `font-size: ${f}`, onClick: toggle },
         children,
         " ",
         x));
--- components/fancify.tsx
+++ components/fancify.tsx
@@ -1,8 +1,12 @@
-import { jsx } from "../framework.js";
+import { IState, jsx } from "../framework.js";
 
-export default function ({ x, children }: { x: any; children?: any }) {
+export default function ({ x, children }: { x: any; children?: any }, { useState }: IState) {
+  const [f, setf] = useState("large");
+
+  const toggle = () => setf(f === "large" ? "small" : "large");
+
   return (
-    <div style="font-size: large">
+    <div style={`font-size: ${f}`} onClick={toggle}>
       {children} {x}
     </div>
   );
